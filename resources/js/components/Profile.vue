96856312166e9edc30bd2093a51a299f57f6109c
mushonnifar <liesmuson@gmail.com>
2018-10-09 11:21:02 +0700

    test page profile

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -4,7 +4,7 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        My Profile
+                        My Profile Testing
                     </div>
 
                     <div class="panel-body">
